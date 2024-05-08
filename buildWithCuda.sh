577b89bd817112870e0ecccf06614057c2393587
tony <292224750@qq.com>
2024-05-08 20:02:51 +0800

    1. first save

--- buildWithCuda.sh
+++ buildWithCuda.sh
@@ -42,14 +42,14 @@ sudo apt-get install -y graphviz
 sudo apt-get install -y libcudnn8 libcudnn8-dev
 pip install matplotlib pandas==2.0.0
 pip install torch==1.13.0
-echo "Build CANDY and PyRania"
+echo "Build CANDY and PyCandy"
 # Step 1: Configure the project
 export CUDACXX=/usr/local/cuda/bin/nvcc
-cmake -S . -B build -DCMAKE_PREFIX_PATH=`python3 -c 'import torch;print(torch.utils.cmake_prefix_path)'` -DENABLE_HDF5=ON -DENABLE_PYBIND=ON -DCMAKE_INSTALL_PREFIX=/usr/local/lib
+mkdir build && cd build &&cmake -DCMAKE_PREFIX_PATH=`python3 -c 'import torch;print(torch.utils.cmake_prefix_path)'` -DENABLE_HDF5=ON -DENABLE_PYBIND=ON -DCMAKE_INSTALL_PREFIX=/usr/local/lib ..
 
 # Step 2: Determine the maximum number of threads
 max_threads=$(nproc)
 
 # Step 3: Build the project using the maximum number of threads
-cmake --build build --parallel $max_threads
-sudo cmake --install build
+cmake --build . --parallel $max_threads
+sudo cmake --install .
