76740478d25040e215ee49aefccc2460623bb64f
tony <292224750@qq.com>
2024-06-04 15:17:52 +0800

    1. fix readme and add one-key build script

--- buildCPUOnly.sh
+++ buildCPUOnly.sh
@@ -4,7 +4,7 @@ echo "First, make sure you have sudo"
 sudo ls
 echo "Installing others..."
 sudo apt install -y liblapack-dev libblas-dev
-sudo apt-get install -y graphviz
+sudo apt-get install -y graphviz libboost-dev
 pip install matplotlib pandas==2.0.0
 pip install torch>=1.13.0 --index-url https://download.pytorch.org/whl/cpu
 echo "Build CANDY and PyCandy"
