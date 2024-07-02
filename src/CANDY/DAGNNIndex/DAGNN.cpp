a4bdeaee4abcead275f152d8915548844b0ab6d2
Rubatozh <qq1034010422@outlook.com>
2024-07-02 21:04:01 +0800

    fix

--- src/CANDY/DAGNNIndex/DAGNN.cpp
+++ src/CANDY/DAGNNIndex/DAGNN.cpp
@@ -74,6 +74,13 @@ void CANDY::DynamicTuneHNSW::add(idx_t n, float* x) {
             graphStates.time_local_stat.value_average[d] += m;
         }
         graphStates.time_local_stat.ntotal+=1;
+        /// Update degree measure, since insert a 0-point into data
+        double old_degree_avg = graphStates.time_local_stat.degree_sum_new*1.0/((graphStates.time_local_stat.ntotal-1)*1.0);
+        double new_degree_avg = graphStates.time_local_stat.degree_sum_new*1.0/(graphStates.time_local_stat.ntotal*1.0);
+        double variance_without = graphStates.time_local_stat.degree_variance_new;
+        double new_degree_variance = (graphStates.time_local_stat.ntotal-1)*1.0/(graphStates.time_local_stat.ntotal*1.0)*(variance_without + (0-old_degree_avg)*(0-old_degree_avg)/graphStates.time_local_stat.ntotal);
+        graphStates.time_local_stat.degree_variance_new = new_degree_variance;
+
         disq.set_query(x+vecDim*i);
 
         auto node = linkLists[i+n0];
