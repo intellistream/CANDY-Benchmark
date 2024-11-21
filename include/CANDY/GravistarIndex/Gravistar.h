8eb5a31ece07609f44f21ce2a861a5fe4925690d
tonyskyzeng <292224750@qq.com>
2024-11-21 17:01:12 +0800

    1. first create

--- include/CANDY/GravistarIndex/Gravistar.h
+++ include/CANDY/GravistarIndex/Gravistar.h
@@ -7,6 +7,7 @@
 #define CANDYBENCH_INCLUDE_CANDY_GRAVISTARINDEX_GRAVISTAR_H_
 #include <CANDY/FlatGPUIndex/DiskMemBuffer.h>
 namespace CANDY {
+class Gravistar_DarkMatter;
 /**
  *  @defgroup  CANDY_lib_bottom_sub The support classes for index approaches
  * @{
@@ -21,22 +22,30 @@ class Gravistar_DarkMatter{
    * @brief this is where we hold data tensors
    */
   PlainMemBufferTU dmBuffer;
+  int64_t bufferSize = -1;
  public:
   Gravistar_DarkMatter() {}
   ~Gravistar_DarkMatter() {}
   /**
    * @brief init everything
    * @param vecDim The dimension of vectors
-   * @param bufferSize the size for both tensor cache (in rows) and  U64 cache (in sizeof(uint64_t))
+   * @param _bufferSize the size for both tensor cache (in rows) and  U64 cache (in sizeof(uint64_t))
    * @param _tensorBegin the begin offset of tensor storage in disk
    * @param _u64Begin the begin offset of u64 storage in disk
    * @param _dmaSize the max size of dma buffer, I64, default 1024000
    */
   void init(int64_t vecDim,
-            int64_t bufferSize,
+            int64_t _bufferSize,
             int64_t _tensorBegin,
             int64_t _u64Begin,
             int64_t _dmaSize = 1024000);
+  /**
+   * @brief insert a tensor
+   * @param t the tensor, accept multiple rows
+   * @return bool whether the insertion is successful, will be false if run out of buffer
+   */
+  virtual bool insertTensor(torch::Tensor &t);
+
 };
 
 
--- src/CANDY/Gravistar/Gravistar.cpp
+++ src/CANDY/Gravistar/Gravistar.cpp
@@ -4,11 +4,21 @@
 #include <CANDY/GravistarIndex/Gravistar.h>
 namespace CANDY {
 void Gravistar_DarkMatter::init(int64_t vecDim,
-                                int64_t bufferSize,
+                                int64_t _bufferSize,
                                 int64_t _tensorBegin,
                                 int64_t _u64Begin,
                                 int64_t _dmaSize) {
-  dmBuffer.init(vecDim,bufferSize,_tensorBegin,_u64Begin,_dmaSize);
+  dmBuffer.init(vecDim,_bufferSize,_tensorBegin,_u64Begin,_dmaSize);
+  bufferSize = _bufferSize;
 }
+
+bool CANDY::Gravistar_DarkMatter::insertTensor(torch::Tensor &t) {
+  if (t.size(0)+dmBuffer.size() > bufferSize) {
+    return false;
+  } else {
+    return dmBuffer.appendTensor(t);
+  }
+}
+
 
 }
