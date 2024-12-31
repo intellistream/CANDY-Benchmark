1bc854b5e605cd314eea3823c69d686f3b173ddc
rubatozh <qq1034010422@outlook.com>
2025-01-01 01:30:29 +0800

    dependency fix

--- buildCPUOnly.sh
+++ buildCPUOnly.sh
@@ -5,7 +5,7 @@ sudo ls
 echo "Installing others..."
 sudo apt install -y liblapack-dev libblas-dev
 sudo apt-get install -y graphviz libboost-all-dev swig libgtest-dev
-sudo apt-get install libgoogle-glog-dev libgoogle-gflags-dev
+sudo apt-get install libgoogle-glog-dev libgflags-dev
 pip install matplotlib pandas==2.0.0
 pip install torch==2.4.0 --index-url https://download.pytorch.org/whl/cpu
 echo "Build CANDY and PyCandy"
